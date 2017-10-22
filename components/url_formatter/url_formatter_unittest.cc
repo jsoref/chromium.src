3ce94943c3d35933e2533fca4a2a0863c3df3118
Josh Soref <jsoref@users.noreply.github.com>
2017-10-22 21:55:06 +0000

    spelling: arabic

--- components/url_formatter/url_formatter_unittest.cc
+++ components/url_formatter/url_formatter_unittest.cc
@@ -614,7 +614,7 @@ const IDNTestCase idn_cases[] = {
     // Block RTL nonspacing marks (NSM) after unrelated scripts.
     {"xn--foog-ycg.com", L"foog\x0650.com", false},    // Latin + Arabic NSM
     {"xn--foog-jdg.com", L"foog\x0654.com", false},    // Latin + Arabic NSM
-    {"xn--foog-jhg.com", L"foog\x0670.com", false},    // Latin + Arbic NSM
+    {"xn--foog-jhg.com", L"foog\x0670.com", false},    // Latin + Arabic NSM
     {"xn--foog-opf.com", L"foog\x05b4.com", false},    // Latin + Hebrew NSM
     {"xn--shb5495f.com", L"\xac00\x0650.com", false},  // Hang + Arabic NSM
 
